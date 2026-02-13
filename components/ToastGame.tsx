c79d16a1391f445f9c23c80f7cecadbad2ec58d1
FengFang Chen <142323712+n1f2s@users.noreply.github.com>
2026-02-13 11:22:32 +0800

    feat: Host maotai image on GitHub CDN
    The `maotai.png` image has been moved from the local `public/assets` directory to a GitHub CDN. This change updates the `ToastGame.tsx` component to use the new URL for the image. This will improve loading performance and reduce the size of the deployed application bundle.

--- components/ToastGame.tsx
+++ components/ToastGame.tsx
@@ -127,7 +127,7 @@ export const ToastHUD: React.FC<ToastHUDProps> = ({ fillLevel, onPourStart, onPo
                     */}
                     {!imgError ? (
                         <img 
-                            src="/assets/maotai.png" 
+                            src="https://raw.githubusercontent.com/n1f2s/ShaDong-Master/main/maotai.png" 
                             alt="Moutai Bottle" 
                             className="w-full h-full object-contain drop-shadow-2xl pointer-events-none"
                             draggable={false}
